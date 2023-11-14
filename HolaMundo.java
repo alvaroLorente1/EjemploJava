f48eee62a426252cd8d1f7c64f4774bade4cdb08
Álvaro Lorente Almán <lorente.alalv24@triana.salesianos.edu>
2023-11-14 13:55:13 +0100

    Cambio al holaMundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,8 +1,10 @@
 public class HolaMundo {
     public static void main(String[] args) {
         // Variables y constantes
-
+        System.out.println("*".repeat(10));
         System.out.println("HOLA MUNDO");
+        System.out.println("*".repeat(10));
+        
 
     }
 
